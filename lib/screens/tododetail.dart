57b3164515e74c1b40bc05cce9d7eed2397e173a
Gabrielle Gaudeau <gabriellegaudeau@hotmail.co.uk>
2020-06-12 10:01:37 +0100

    tododetail build

---
+++ lib/screens/tododetail.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/util/dbhelper.dart';
+import 'package:intl/intl.dart';
+
+class TodoDetail extends StatefulWidget {
+  final Todo todo;
+  TodoDetail(this.todo);
+
+  @override
+  State<StatefulWidget> createState() => TodoDetailState(todo);
+}
+
+class TodoDetailState extends State {
+  Todo todo;
+  TodoDetailState(this.todo);
+  final _priorities = ["High", "Medium", "Low"];
+  String _priority = "Low";
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    titleController.text = todo.title;
+    descriptionController.text = todo.description;
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+    return new Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(todo.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: titleController,
+            style: textStyle,
+            decoration: InputDecoration(
+              labelText: "Title",
+              labelStyle: textStyle,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+          TextField(
+            controller: descriptionController,
+            style: textStyle,
+            decoration: InputDecoration(
+              labelText: "Description",
+              labelStyle: textStyle,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+          DropdownButton<String>(
+            items: _priorities.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            style: textStyle,
+            value: "Low",
+            onChanged: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
